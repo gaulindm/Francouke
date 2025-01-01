d451f572c58de53d296766b42786448fb65cf034
Daniel Gaulin <gaulindm@gmail.com>
2025-01-01 06:19:37 -0500

    Before trying to fix diagrams at bottom

--- songbook/admin.py
+++ songbook/admin.py
@@ -4,11 +4,20 @@ from .models import Song
 
 @admin.register(Song)
 class YourModelAdmin(admin.ModelAdmin):
-    list_display = ['songTitle', 'get_artist', 'get_tags']
-    search_fields = ['songTitle', 'metadata__artist','abc_notation']
+    list_display = ['songTitle', 'get_artist', 'get_year',  'get_tags']
+    search_fields = ['songTitle', 'metadata__artist', 'get_artist','abc_notation']
     
     
     ordering = ('metadata__artist',)
+
+    def get_year(self, obj):
+        # Extract the artist from the metadata JSON field
+        return obj.metadata.get('year', 'Unknown') if obj.metadata else 'No Metadata'
+    get_year.admin_order_field = 'metadata__year'
+    get_year.short_description = 'Year'  # Column title in the admin
+
+
+
 
     def get_artist(self, obj):
         # Extract the artist from the metadata JSON field
